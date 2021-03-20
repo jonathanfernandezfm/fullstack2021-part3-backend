2ce9e8beeff64531a1ba5508449f52e85b6caaa1
Jonathan <jonathanfernandezfm@gmail.com>
2021-03-20 17:58:41 +0100

    Front end added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', function (req, res) {
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body '));
 app.use(express.json());
+app.use(express.static('build'));
 app.use(cors());
 
 let persons = [
